0e78f97f7b258cdad76ac61ba5c993ffb66d1462
mrun1corn <robinisalegend01@gmail.com>
2024-03-31 13:28:18 +0600

    edited as my preference

--- data/data.js
+++ data/data.js
@@ -10,22 +10,22 @@
 //=========================================
 export const introductionData = {
     // Hi, my name is....
-    yourName: "Your Name", // Insert your name
+    yourName: "Robin", // Insert your name
     socialNetwork: [
         {
             icon: "fa fa-envelope-o",  // find other icon in https://fontawesome.com/v4.7/icons/
             nameSocialNetwork: "Email",  // label name
-            url: "#"  //  url of the Social Network
+            url: "robin424123@gmail.com"  //  url of the Social Network
         },
         {
-            icon: "fa fa-linkedin-square", // find other icon in https://fontawesome.com/v4.7/icons/
-            nameSocialNetwork: "Linkedin",
-            url: "#"
+            icon: "fa fa-facebook-square", // find other icon in https://fontawesome.com/v4.7/icons/
+            nameSocialNetwork: "Facebook",
+            url: "facebook.com/mrun1orn"
         },
         {
             icon: "fa fa-github-alt",   // find other icon in https://fontawesome.com/v4.7/icons/
             nameSocialNetwork: "Github",
-            url: "#"
+            url: "github.com/mrun1corn"
         }
 
 
@@ -56,8 +56,8 @@ export const introductionData = {
 //                ABOUT ME
 //=========================================
 export const aboutMeData = {
-    title: "Example",
-    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. A ratione harum sapiente ipsum maiores, blanditiis atque magni quam, quia quas hic deserunt rem aliquam. Error earum ab modi porro at? Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus sunt veniam, eligendi modi similique tempore accusamus dolore, nobis non facilis quos veritatis officia eveniet molestias libero quisquam sed doloremque fugit!",
+    title: "About Me",
+    description: "Hello there! I'm Robin, an Assistant Network Engineer with a passion for technology and a knack for self-learning. I thrive on the challenges of troubleshooting network issues and optimizing systems for peak performance. Whether it's configuring routers, ensuring network security, or staying updated with the latest advancements in the field, I'm always eager to expand my knowledge and skills. Outside of work, you can find me diving into online courses, experimenting with new networking concepts, or simply enjoying a good book on technology. Let's connect and explore the ever-evolving world of networking together!",
     titleURL: "View CV",
     urlCV: "#"
 }
@@ -72,21 +72,17 @@ export const skillsData = [
 
 
     {
-        img: "assets/python.svg",  // card image  > insert the svg image in the assets folder
-        nameSkill: "Python"      //  card name
+        img: "assets/ru.png",  // card image  > insert the svg image in the assets folder
+        nameSkill: "Ubuntu"      //  card name
     },
     {
-        img: "assets/java.svg",
-        nameSkill: "Java"
+        img: "assets/m.png",
+        nameSkill: "Router"
     },
     {
-        img: "assets/js.svg",
-        nameSkill: "JavaScript"
+        img: "assets/a.png",
+        nameSkill: "Android Rom Development"
     },
-    {
-        img: "assets/js.svg",
-        nameSkill: "JavaScript"
-    }
 
 ]
 //=========================================
@@ -94,23 +90,18 @@ export const skillsData = [
 //=========================================
 export const projectsData = [
     {
-        title: "PROJECT TITLE",    // card image  > insert the svg image in the assets folder
-        describe: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam veritatis iusto reiciendisquos, delectus in deserunt harum a. Rerum similique corporis sit odit error incidunt distinctio consectetur neque, cum illum?",
-        url: "#",  // url of the Github repository
+        title: "Bash Scripting",    // card image  > insert the svg image in the assets folder
+        describe: "Proficient in Bash scripting, I utilize this skill to automate various tasks, streamline processes, and enhance efficiency within network environments. I have hands-on experience creating custom scripts tailored to specific needs, whether it's automating routine maintenance tasks, deploying software packages, or configuring network devices.",
+        url: "github.com/mrun1corn/server_scripts",  // url of the Github repository
         img: "assets/project1.jpg"  //image Project
     },
     {
-        title: "PROJECT TITLE",
-        describe: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam veritatis iusto reiciendisquos, delectus in deserunt harum a. Rerum similique corporis sit odit error incidunt distinctio consectetur neque, cum illum?",
+        title: "Hypervisor Technologies",
+        describe: "Virtualization technology has become increasingly prevalent in modern computing environments, enabling efficient resource utilization, enhanced security, and simplified infrastructure management. Hypervisors play a pivotal role in virtualization by abstracting physical hardware resources and facilitating the creation and management of virtual machines (VMs). This project aims to conduct a comprehensive comparative analysis of prominent hypervisor technologies, including VirtualBox, VMware, and Cockpit, to evaluate their features, performance, and suitability for various use cases.",
         url: "#",
-        img: "assets/project1.jpg"
+        img: "assets/h.jpg"
     },
-    {
-        title: "PROJECT TITLE",
-        describe: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam veritatis iusto reiciendisquos, delectus in deserunt harum a. Rerum similique corporis sit odit error incidunt distinctio consectetur neque, cum illum?",
-        url: "#",
-        img: "assets/project1.jpg"
-    }
+
 ]
 
 
